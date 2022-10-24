0be4ecb5c1538a4731629ae2fb0483a747d1d1ef
sjsone <simon@sjs.one>
2022-10-24 20:06:49 +0200

    Improved Autocompletion

--- server/src/capabilities/CompletionCapability.ts
+++ server/src/capabilities/CompletionCapability.ts
@@ -1,7 +1,9 @@
+import { ObjectNode } from 'ts-fusion-parser/out/eel/nodes/ObjectNode'
+import { ObjectPathNode } from 'ts-fusion-parser/out/eel/nodes/ObjectPathNode'
 import { FusionObjectValue } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/FusionObjectValue'
 import { PathSegment } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/PathSegment'
 import { PrototypePathSegment } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/PrototypePathSegment'
-import { CompletionItem, CompletionItemKind, InsertTextMode, TextDocumentPositionParams } from 'vscode-languageserver/node'
+import { CompletionItem, CompletionItemKind, InsertReplaceEdit, InsertTextMode, Position, TextDocumentPositionParams } from 'vscode-languageserver/node'
 import { FusionWorkspace } from '../fusion/FusionWorkspace'
 import { ParsedFusionFile } from '../fusion/ParsedFusionFile'
 import { LinePositionedNode } from '../LinePositionedNode'
@@ -24,6 +26,9 @@ export class CompletionCapability extends AbstractCapability {
 				case foundNode instanceof FusionObjectValue:
 				case foundNode instanceof PrototypePathSegment:
 					completions.push(...this.getPrototypeCompletions(fusionWorkspace, foundLinePositionedNode))
+					break;
+				case foundNode instanceof ObjectPathNode:
+					completions.push(...this.getObjectPathNodeCompletions(fusionWorkspace, foundLinePositionedNode))
 					break;
 			}
 		}
@@ -67,13 +72,51 @@ export class CompletionCapability extends AbstractCapability {
 						kind: CompletionItemKind.Class,
 						insertTextMode: InsertTextMode.asIs,
 						insertText: label,
-						range: {
-							start: { line: foundNode.getBegin().line - 1, character: foundNode.getBegin().column - 1 },
-							end: { line: foundNode.getBegin().line - 1, character: foundNode.getBegin().column + label.length - 1 },
-						},
-						newText: label
+						textEdit: {
+							range: {
+								start: { line: foundNode.getBegin().line - 1, character: foundNode.getBegin().column - 1 },
+								end: { line: foundNode.getBegin().line - 1, character: foundNode.getBegin().column + label.length - 1 },
+							},
+							newText: label
+						}
 					})
 				}
+			}
+		}
+
+		return completions
+	}
+
+	protected getObjectPathNodeCompletions(fusionWorkspace: FusionWorkspace, foundNode: LinePositionedNode<any>): CompletionItem[] {
+		const node = <ObjectPathNode>foundNode.getNode()
+		const objectNode = <ObjectNode>node["parent"]
+		const linePositionedObjectNode = objectNode["linePositionedNode"]
+		const fullPath = objectNode["path"].map(part => part["value"]).join(".")
+		const completions: CompletionItem[] = []
+
+		const eelHelpers = fusionWorkspace.neosWorkspace.getEelHelperTokens()
+		for(const eelHelper of eelHelpers) {
+			for(const method of eelHelper.methods) {
+				const fullName = eelHelper.name + "." + method.name
+				if(!fullName.startsWith(fullPath)) continue
+				
+				const label = fullName
+				const insertReplaceEdit: InsertReplaceEdit = {
+					insert: linePositionedObjectNode.getPositionAsRange(),
+					replace: {
+						start: { line: linePositionedObjectNode.getBegin().line - 1, character: linePositionedObjectNode.getBegin().column - 1 },
+						end: { line: linePositionedObjectNode.getEnd().line - 1, character: linePositionedObjectNode.getEnd().column + label.length - 1 },
+					},
+					newText: label
+				}
+
+				completions.push({
+					label,
+					kind: CompletionItemKind.Method,
+					insertTextMode: InsertTextMode.adjustIndentation,
+					insertText: label,
+					textEdit: insertReplaceEdit
+				})
 			}
 		}
 
