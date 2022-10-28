2513cf6dd0e0040cede72ab34a17e94058e271e5
sjsone <simon@sjs.one>
2022-10-28 17:22:41 +0200

    Cleanup

--- server/src/test.ts
+++ server/src/test.ts
@@ -1,17 +1,2 @@
 import * as NodeFs from "fs"
 import * as NodePath from "path"
-import { FusionObjectValue } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/FusionObjectValue'
-import { FusionWorkspace } from './fusion/FusionWorkspace'
-import { getLineNumberOfChar } from './util'
-
-
-
-const filePath = "/Users/simon/Downloads/bauwerk-capital-website-master/source/DistributionPackages/BauwerkCapital.Website/Classes/Eel/Helper/SearchHelper.php"
-
-const file = NodeFs.readFileSync(filePath).toString()
-
-
-const start = 22818 
-const end = 22856
-console.log(file.substring(start, end))
-// console.log(getLineNumberOfChar(file, start, true))
