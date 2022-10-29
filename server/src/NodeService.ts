92a241c0facf386aa6e2d23d49501fcc09c1490b
sjsone <simon@sjs.one>
2022-10-29 15:26:06 +0200

    Improved findPropertyDefinitionSegments

--- server/src/NodeService.ts
+++ server/src/NodeService.ts
@@ -5,14 +5,27 @@ import { FusionFile } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/Fus
 import { MetaPathSegment } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/MetaPathSegment'
 import { ObjectStatement } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/ObjectStatement'
 import { PathSegment } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/PathSegment'
+import { PrototypePathSegment } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/PrototypePathSegment'
 import { StatementList } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/StatementList'
 import { ValueAssignment } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/ValueAssignment'
+import { ValueCopy } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/ValueCopy'
+import { FusionWorkspace } from './fusion/FusionWorkspace'
 import { findParent } from './util'
+
+export class ExternalObjectStatement {
+	statement: ObjectStatement
+	uri?: string
+
+	constructor(statement: ObjectStatement, uri: string) {
+		this.statement = statement
+		this.uri = uri
+	}
+}
 
 class NodeService {
 
 
-	public * findPropertyDefinitionSegments(objectNode: ObjectNode) {
+	public * findPropertyDefinitionSegments(objectNode: ObjectNode, workspace?: FusionWorkspace) {
 		const objectStatement = findParent(objectNode, ObjectStatement) // [props.foo]
 
 		let wasComingFromRenderer = false
@@ -25,18 +38,36 @@ class NodeService {
 
 		let traverseUpwards = true
 		let skipNextStatements = false
-		let onlyApply = false
-		let inPrototypeSegmentList = false
+		let onlyWhenFoundApplyProps = false
 
 		do {
-			inPrototypeSegmentList = statementList["parent"] instanceof FusionFile
-			if (!onlyApply && wasComingFromRenderer) onlyApply = true
+			if (!onlyWhenFoundApplyProps && wasComingFromRenderer) onlyWhenFoundApplyProps = true
 
 			const parentObjectNode = findParent(statementList, ObjectStatement)
-			const parentObjectIdentifier = parentObjectNode.path.segments[0]["identifier"]
+			const parentObjectIdentifier = parentObjectNode ? parentObjectNode.path.segments[0]["identifier"] : ""
 			let foundApplyProps = false
 
-			for (const statement of statementList.statements) {
+			const statements: Array<ObjectStatement | ExternalObjectStatement> = [...<ObjectStatement[]>statementList.statements]
+			if (workspace !== undefined) {
+				const parentStatementList = findParent(statementList, StatementList)
+				if (parentStatementList) {
+					const willBeInPrototypeSegmentList = parentStatementList["parent"] instanceof FusionFile
+					if (willBeInPrototypeSegmentList) {
+						const operation = findParent(statementList, ObjectStatement).operation
+						if (operation instanceof ValueCopy) {
+							const prototypeSegment = operation.assignedObjectPath.objectPath.segments[0]
+							if (prototypeSegment instanceof PrototypePathSegment) {
+								statements.push(...this.getInheritedPropertiesByPrototypeName(prototypeSegment.identifier, workspace))
+							}
+						}
+					}
+				}
+			}
+
+			for (const statement of statements) {
+				if (statement instanceof ExternalObjectStatement) {
+					yield statement
+				}
 				if (!(statement instanceof ObjectStatement)) continue
 				if (statement === objectStatement) continue // Let it not find itself
 
@@ -47,19 +78,22 @@ class NodeService {
 			skipNextStatements = parentObjectIdentifier !== "renderer"
 			if (!wasComingFromRenderer) wasComingFromRenderer = parentObjectIdentifier === "renderer"
 
-			traverseUpwards = !onlyApply || foundApplyProps
+			traverseUpwards = !onlyWhenFoundApplyProps || foundApplyProps
 			statementList = findParent(statementList, StatementList)
-		} while (traverseUpwards && statementList && !inPrototypeSegmentList)
+		} while (traverseUpwards && statementList && !(statementList["parent"] instanceof FusionFile))
 	}
 
-	public findPropertyDefinitionSegment(objectNode: ObjectNode) {
-		for (const segment of this.findPropertyDefinitionSegments(objectNode)) {
-			if (!(segment instanceof PathSegment)) continue
+	public findPropertyDefinitionSegment(objectNode: ObjectNode, workspace?: FusionWorkspace) {
+		for (const segmentOrExternalStatement of this.findPropertyDefinitionSegments(objectNode, workspace)) {
+			if (segmentOrExternalStatement instanceof ExternalObjectStatement) {
+				if (segmentOrExternalStatement.statement.path.segments[0]["identifier"] === objectNode.path[1]["value"]) return segmentOrExternalStatement
+			}
+			if (!(segmentOrExternalStatement instanceof PathSegment)) continue
 			// TODO: Decide what to do with "renderer"
-			if (segment.identifier === "renderer") continue
+			if (segmentOrExternalStatement.identifier === "renderer") continue
 
-			if (objectNode.path.length > 1 && segment.identifier === objectNode.path[1]["value"]) {
-				return segment
+			if (objectNode.path.length > 1 && segmentOrExternalStatement.identifier === objectNode.path[1]["value"]) {
+				return segmentOrExternalStatement
 			}
 		}
 
@@ -80,6 +114,30 @@ class NodeService {
 		return appliedObjectNode.path[0]["value"] === "props"
 	}
 
+	public getInheritedPropertiesByPrototypeName(name: string, workspace: FusionWorkspace) {
+		const statements: Array<ExternalObjectStatement> = []
+		for (const otherParsedFile of workspace.parsedFiles) {
+			for (const positionedNode of [...otherParsedFile.prototypeCreations]) {
+				if (positionedNode.getNode()["identifier"] !== name) continue
+				const node = positionedNode.getNode()
+				const objectStatement = findParent(node, ObjectStatement)
+				const operation = objectStatement.operation
+				if (operation instanceof ValueCopy) {
+					const prototypeSegment = operation.assignedObjectPath.objectPath.segments[0]
+					if (prototypeSegment instanceof PrototypePathSegment) {
+						statements.push(...this.getInheritedPropertiesByPrototypeName(prototypeSegment.identifier, workspace))
+					}
+				}
+
+				for (const statement of objectStatement.block.statementList.statements) {
+					if (!(statement instanceof ObjectStatement)) continue
+					statements.push(new ExternalObjectStatement(statement, otherParsedFile.uri))
+				}
+			}
+		}
+
+		return statements
+	}
 }
 
 const nodeService = new NodeService
