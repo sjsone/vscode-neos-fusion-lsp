b6bc8206642c0cacc4536a6c65bf65a74c832df2
sjsone <simon@sjs.one>
2022-12-04 01:53:38 +0100

    Cleanup

--- server/src/fusion/ParsedFusionFile.ts
+++ server/src/fusion/ParsedFusionFile.ts
@@ -232,14 +232,14 @@ export class ParsedFusionFile {
 			const objectStatement = findParent(node, ObjectStatement)
 			if (objectStatement === undefined) continue
 			if (objectStatement.path.segments[0] instanceof MetaPathSegment) continue
-			const pathBegin = node["path"][0]["value"]
+			const pathBegin = node.path[0]["value"]
 			if (pathBegin !== "props") continue
-			if (node["path"].length === 1) continue
-			if (node["path"][1]["value"] === "content") continue
-			const definition = definitionCapability.getPropertyDefinitions(this, this.workspace, LinePositionedNode.Get(node["path"][0]))
+			if (node.path.length === 1) continue
+			if (node.path[1]["value"] === "content") continue
+			const definition = definitionCapability.getPropertyDefinitions(this, this.workspace, LinePositionedNode.Get(node.path[0]))
 			if (definition) continue
 
-			const objectStatementText = node["path"].map(e => e["value"]).join(".")
+			const objectStatementText = node.path.map(e => e["value"]).join(".")
 			const diagnostic: Diagnostic = {
 				severity: DiagnosticSeverity.Warning,
 				range: positionedObjectNode.getPositionAsRange(),
