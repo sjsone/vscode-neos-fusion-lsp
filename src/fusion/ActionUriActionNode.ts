16ff06bb4f8518054d3d878330d3a79bbafa8a65
sjsone <simon@sjs.one>
2023-01-28 16:15:19 +0100

    fix: correct types in ActionUri Nodes

--- src/fusion/ActionUriActionNode.ts
+++ src/fusion/ActionUriActionNode.ts
@@ -1,10 +1,12 @@
 import { AbstractNode } from 'ts-fusion-parser/out/common/AbstractNode';
 import { ObjectStatement } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/ObjectStatement';
 import { StringValue } from 'ts-fusion-parser/out/fusion/objectTreeParser/ast/StringValue';
+import { ActionUriDefinitionNode } from './ActionUriDefinitionNode';
 
 export class ActionUriActionNode extends AbstractNode {
 	statement: ObjectStatement
 	name: StringValue
+	parent: ActionUriDefinitionNode
 
 	constructor(statement: ObjectStatement, name: StringValue) {
 		super(statement["position"], undefined)
