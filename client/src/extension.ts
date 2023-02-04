eecdd780e2466dcb8c440df31a3c22c4bcdbff80
sjsone <simon@sjs.one>
2023-02-04 23:11:00 +0100

    chore: command to restart server for inspection

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 import {
-	workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri, workspace, ExtensionMode
+	workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri, workspace, ExtensionMode, commands
 } from 'vscode'
 
 import {
@@ -9,6 +9,8 @@ import {
 import { PreferenceService } from './preferenceService'
 import { ProgressNotificationService } from './progressNotificationService'
 import { StatusItemService } from './statusItemService'
+
+// TODO: Create own extension class 
 
 const clients: Map<string, LanguageClient> = new Map()
 
@@ -41,8 +43,50 @@ function getOuterMostWorkspaceFolder(folder: WorkspaceFolder): WorkspaceFolder {
 	return folder
 }
 
-export function activate(context: ExtensionContext) {
+function startClient(context: ExtensionContext, folder: WorkspaceFolder, outputChannel: OutputChannel, inspect: boolean = false) {
 	const module = context.asAbsolutePath(path.join('server', 'out', 'main.js'))
+
+	const debugOptions = { execArgv: ["--nolazy", `--inspect-brk=${6011 + clients.size}`] }
+	const runOptions = { execArgv: [] }
+	if (inspect) {
+		runOptions.execArgv.push(`--inspect-brk=${6011 + clients.size}`)
+	}
+	const serverOptions: ServerOptions = {
+		run: { module, transport: TransportKind.ipc, options: runOptions },
+		debug: { module, transport: TransportKind.ipc, options: debugOptions }
+	}
+	const documentSelector = [{ scheme: 'file', language: 'fusion', pattern: `${folder.uri.fsPath}/**/*` }]
+	const clientOptions: LanguageClientOptions = {
+		documentSelector,
+		diagnosticCollectionName: 'vscode-neos-fusion-lsp',
+		workspaceFolder: folder,
+		outputChannel: outputChannel,
+		synchronize: {
+			configurationSection: 'neosFusionLsp',
+			fileEvents: [
+				workspace.createFileSystemWatcher('**/*.php'),
+				workspace.createFileSystemWatcher('**/*.fusion')
+			]
+		},
+	}
+
+	const statusItemService = new StatusItemService(documentSelector)
+	const progressNotificationService = new ProgressNotificationService()
+	const client = new LanguageClient('vscode-neos-fusion-lsp', 'LSP For Neos Fusion (and AFX)', serverOptions, clientOptions)
+
+	client.onNotification('custom/busy/create', ({ id, configuration }) => statusItemService.createStatusItem(id, configuration))
+	client.onNotification('custom/progressNotification/create', ({ id, title }) => progressNotificationService.create(id, title))
+
+	client.onNotification('custom/progressNotification/update', ({ id, payload }) => progressNotificationService.update(id, payload))
+
+	client.onNotification('custom/busy/dispose', ({ id }) => statusItemService.disposeStatusItem(id))
+	client.onNotification('custom/progressNotification/finish', ({ id }) => progressNotificationService.finish(id))
+
+	client.start()
+	clients.set(folder.uri.toString(), client)
+}
+
+export function activate(context: ExtensionContext) {
 	const outputChannel: OutputChannel = Window.createOutputChannel('Neos Fusion LSP')
 
 	if (workspace.getConfiguration().get("neosFusionLsp.extensions.modify", false)) {
@@ -54,63 +98,34 @@ export function activate(context: ExtensionContext) {
 		})
 	}
 
+	const inspectCommandHandler = async (name: string = 'world') => {
+		const uris = Array.from(clients.keys())
 
+		await stopClients()
+
+		for (const uri of uris) {
+			const folder = Workspace.getWorkspaceFolder(Uri.file(uri.replace("file://", "")))
+			if (!folder) continue
+
+			const outerMostWorkspaceFolder = getOuterMostWorkspaceFolder(folder)
+			startClient(context, outerMostWorkspaceFolder, outputChannel, true)
+		}
+	}
+
+	context.subscriptions.push(commands.registerCommand('neos-fusion-lsp.inspect', inspectCommandHandler));
 
 	function didOpenTextDocument(document: TextDocument): void {
-		if (document.languageId !== 'fusion' || (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled')) {
-			return
-		}
+		if (document.languageId !== 'fusion' || (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled')) return
 
 		const uri = document.uri
+		const folder = Workspace.getWorkspaceFolder(uri)
+		if (!folder) return
 
-		let folder = Workspace.getWorkspaceFolder(uri)
-		if (!folder) {
-			return
-		}
+		const outerMostWorkspaceFolder = getOuterMostWorkspaceFolder(folder)
+		if (clients.has(outerMostWorkspaceFolder.uri.toString())) return
 
-		folder = getOuterMostWorkspaceFolder(folder)
-
-		if (!clients.has(folder.uri.toString())) {
-			const debugOptions = { execArgv: ["--nolazy", `--inspect-brk=${6011 + clients.size}`] }
-			const runOptions = { execArgv: [] }
-			if (context.extensionMode === ExtensionMode.Development && process.env.SERVER_INSPECT_BREAK === "true") {
-				runOptions.execArgv.push(`--inspect-brk=${6011 + clients.size}`)
-			}
-			const serverOptions: ServerOptions = {
-				run: { module, transport: TransportKind.ipc, options: runOptions },
-				debug: { module, transport: TransportKind.ipc, options: debugOptions }
-			}
-			const documentSelector = [{ scheme: 'file', language: 'fusion', pattern: `${folder.uri.fsPath}/**/*` }]
-			const clientOptions: LanguageClientOptions = {
-				documentSelector,
-				diagnosticCollectionName: 'vscode-neos-fusion-lsp',
-				workspaceFolder: folder,
-				outputChannel: outputChannel,
-				synchronize: {
-					configurationSection: 'neosFusionLsp',
-					fileEvents: [
-						workspace.createFileSystemWatcher('**/*.php'),
-						workspace.createFileSystemWatcher('**/*.fusion')
-					]
-				}
-			}
-
-			const statusItemService = new StatusItemService(documentSelector)
-			const progressNotificationService = new ProgressNotificationService()
-			const client = new LanguageClient('vscode-neos-fusion-lsp', 'LSP For Neos Fusion (and AFX)', serverOptions, clientOptions)
-
-			client.onNotification('custom/busy/create', ({ id, configuration }) => statusItemService.createStatusItem(id, configuration))
-			client.onNotification('custom/progressNotification/create', ({ id, title }) => progressNotificationService.create(id, title))
-
-			client.start()
-			clients.set(folder.uri.toString(), client)
-
-			client.onNotification('custom/progressNotification/update', ({ id, payload }) => progressNotificationService.update(id, payload))
-
-
-			client.onNotification('custom/busy/dispose', ({ id }) => statusItemService.disposeStatusItem(id))
-			client.onNotification('custom/progressNotification/finish', ({ id }) => progressNotificationService.finish(id))
-		}
+		const inspect = workspace.getConfiguration().get("neosFusionLsp.logging.inspect", false)
+		startClient(context, outerMostWorkspaceFolder, outputChannel, inspect)
 	}
 
 	Workspace.onDidOpenTextDocument(didOpenTextDocument)
@@ -126,11 +141,15 @@ export function activate(context: ExtensionContext) {
 	})
 }
 
-export function deactivate(): Thenable<void> {
+function stopClients() {
 	const promises: Thenable<void>[] = []
 
 	for (const client of clients.values()) {
 		promises.push(client.stop())
 	}
 	return Promise.all(promises).then(() => undefined)
+}
+
+export function deactivate(): Thenable<void> {
+	return stopClients()
 }
