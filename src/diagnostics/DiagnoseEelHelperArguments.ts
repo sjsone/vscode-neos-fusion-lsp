f6c04ee1028e46dd0ddc50bde6ebf8702955f4ff
sjsone <simon@sjs.one>
2023-02-11 15:15:48 +0100

    chore: code cleanup

--- src/diagnostics/DiagnoseEelHelperArguments.ts
+++ src/diagnostics/DiagnoseEelHelperArguments.ts
@@ -1,11 +1,45 @@
 import { ObjectFunctionPathNode } from 'ts-fusion-parser/out/dsl/eel/nodes/ObjectFunctionPathNode'
 import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver'
+import { LinePositionedNode } from '../common/LinePositionedNode'
 import { ParsedFusionFile } from '../fusion/ParsedFusionFile'
 import { PhpClassMethodNode } from '../fusion/PhpClassMethodNode'
+import { EELHelperToken } from '../neos/NeosPackage'
 import { CommonDiagnosticHelper } from './CommonDiagnosticHelper'
+
+function* getDiagnosticFromEelHelper(positionedNode: LinePositionedNode<PhpClassMethodNode>, pathNode: ObjectFunctionPathNode, eelHelper: EELHelperToken) {
+	const node = positionedNode.getNode()
+	if (eelHelper.name !== node.eelHelper.identifier) return
+	const method = eelHelper.methods.find(method => method.valid(node.identifier))
+	if (!method) return
+
+	for (const parameterIndex in method.parameters) {
+		const parameter = method.parameters[parameterIndex]
+		if (parameter.defaultValue !== undefined) break
+		if (pathNode.args[parameterIndex] === undefined) {
+			yield {
+				severity: DiagnosticSeverity.Error,
+				range: positionedNode.getPositionAsRange(),
+				message: `Missing argument`,
+				source: CommonDiagnosticHelper.Source
+			} as Diagnostic
+		}
+	}
+
+	if (pathNode.args.length > method.parameters.length) {
+		for (const exceedingArgument of pathNode.args.slice(method.parameters.length)) {
+			yield {
+				severity: DiagnosticSeverity.Warning,
+				range: exceedingArgument.linePositionedNode.getPositionAsRange(),
+				message: `Too many arguments provided`,
+				source: CommonDiagnosticHelper.Source
+			} as Diagnostic
+		}
+	}
+}
 
 export function diagnoseEelHelperArguments(parsedFusionFile: ParsedFusionFile) {
 	const diagnostics: Diagnostic[] = []
+
 	const positionedNodes = parsedFusionFile.getNodesByType(PhpClassMethodNode)
 	if (!positionedNodes) return diagnostics
 	for (const positionedNode of positionedNodes) {
@@ -13,35 +47,11 @@ export function diagnoseEelHelperArguments(parsedFusionFile: ParsedFusionFile) {
 		const pathNode = node.pathNode
 		if (!(pathNode instanceof ObjectFunctionPathNode)) continue
 
+		console.log(pathNode)
+
 		for (const eelHelper of parsedFusionFile.workspace.neosWorkspace.getEelHelperTokens()) {
-			if (eelHelper.name !== node.eelHelper.identifier) continue
-			const method = eelHelper.methods.find(method => method.valid(node.identifier))
-			if (!method) continue
-
-			for (const parameterIndex in method.parameters) {
-				const parameter = method.parameters[parameterIndex]
-				if (parameter.defaultValue !== undefined) break
-				if (pathNode.args[parameterIndex] === undefined) {
-					const diagnostic: Diagnostic = {
-						severity: DiagnosticSeverity.Error,
-						range: positionedNode.getPositionAsRange(),
-						message: `Missing argument`,
-						source: CommonDiagnosticHelper.Source
-					}
-					diagnostics.push(diagnostic)
-				}
-			}
-
-			if (pathNode.args.length > method.parameters.length) {
-				for (const exceedingArgument of pathNode.args.slice(method.parameters.length)) {
-					const diagnostic: Diagnostic = {
-						severity: DiagnosticSeverity.Warning,
-						range: exceedingArgument.linePositionedNode.getPositionAsRange(),
-						message: `Too many arguments provided`,
-						source: CommonDiagnosticHelper.Source
-					}
-					diagnostics.push(diagnostic)
-				}
+			for (const diagnostic of getDiagnosticFromEelHelper(positionedNode, pathNode, eelHelper)) {
+				diagnostics.push(diagnostic)
 			}
 		}
 	}
