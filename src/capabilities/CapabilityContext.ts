f107f5929efa4e3b07f5457363f2d17fc79584aa
sjsone <simon@sjs.one>
2023-03-04 21:40:17 +0100

    chore: improved typing

--- src/capabilities/CapabilityContext.ts
+++ src/capabilities/CapabilityContext.ts
@@ -4,7 +4,7 @@ import { FusionWorkspace } from '../fusion/FusionWorkspace'
 import { ParsedFusionFile } from '../fusion/ParsedFusionFile'
 import { LinePositionedNode } from '../common/LinePositionedNode'
 
-export type CapabilityContext<N extends AbstractNode> = ParsedFileCapabilityContext<N> | WorkspacesCapabilityContext
+export type CapabilityContext<N extends AbstractNode = AbstractNode> = ParsedFileCapabilityContext<N> | WorkspacesCapabilityContext
 
 export interface WorkspacesCapabilityContext {
 	workspaces: FusionWorkspace[],
