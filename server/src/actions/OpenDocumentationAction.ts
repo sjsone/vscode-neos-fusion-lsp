898cc09e83cfc983b790cfd0e683a258c044d415
sjsone <simon@sjs.one>
2023-06-05 22:34:15 +0200

    fix: error in openDocumentationAction

--- server/src/actions/OpenDocumentationAction.ts
+++ server/src/actions/OpenDocumentationAction.ts
@@ -5,9 +5,9 @@ export const openDocumentationAction = (languageServer: LanguageServer, params: 
 	const codeActions: CodeAction[] = [];
 
 	for (const diagnostic of params.context.diagnostics) {
-		if (diagnostic.data?.documentation.uri === undefined) continue
+		if (diagnostic.data?.documentation?.uri === undefined) continue
 
-		if (diagnostic.data.documentation.openInBrowser) codeActions.push({
+		if (diagnostic.data.documentation?.openInBrowser) codeActions.push({
 			title: "Open Documentation in Browser",
 			kind: CodeActionKind.QuickFix,
 			command: Command.create("Open", "vscode.open", diagnostic.data.documentation.uri),
