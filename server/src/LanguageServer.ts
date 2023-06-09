e065d31f7778ff99ca428c185b8a77244440477a
sjsone <simon@sjs.one>
2023-06-09 21:05:42 +0200

    chore: moved file change handling into own handlers

--- server/src/LanguageServer.ts
+++ server/src/LanguageServer.ts
@@ -1,4 +1,4 @@
-import { CodeAction, CodeActionParams, FileEvent } from 'vscode-languageserver'
+import { CodeAction, CodeActionParams } from 'vscode-languageserver'
 import {
 	DidChangeConfigurationParams,
 	DidChangeWatchedFilesParams,
@@ -28,13 +28,17 @@ import { WorkspaceSymbolCapability } from './capabilities/WorkspaceSymbolCapabil
 import { AbstractFunctionality } from './common/AbstractFunctionality'
 import { ClientCapabilityService } from './common/ClientCapabilityService'
 import { LogService, Logger } from './common/Logging'
-import { clearLineDataCache, clearLineDataCacheForFile, uriToPath } from './common/util'
+import { clearLineDataCache, uriToPath } from './common/util'
+import { AbstractFileChangeHandler } from './fileChangeHandler/AbstractFileChangeHandler'
+import { FusionFileChangeHandler } from './fileChangeHandler/FusionFileChangeHandler'
+import { PhpFileChangeHandler } from './fileChangeHandler/PhpFileChangeHandler'
+import { XlfFileChangeHandler } from './fileChangeHandler/XlfFileChangeHandler'
+import { YamlFileChangeHandler } from './fileChangeHandler/YamlFileChangeHandler'
 import { FusionWorkspace } from './fusion/FusionWorkspace'
 import { AbstractLanguageFeature } from './languageFeatures/AbstractLanguageFeature'
 import { InlayHintLanguageFeature } from './languageFeatures/InlayHintLanguageFeature'
 import { SemanticTokensLanguageFeature } from './languageFeatures/SemanticTokensLanguageFeature'
 import { FusionDocument } from './main'
-import { XLIFFTranslationFile } from './translations/XLIFFTranslationFile'
 
 
 const CodeActions = [
@@ -42,6 +46,13 @@ const CodeActions = [
 	replaceDeprecatedQuickFixAction,
 	openDocumentationAction,
 	createNodeTypeFileAction,
+]
+
+const FileChangeHandlerTypes: Array<new (...any) => AbstractFileChangeHandler> = [
+	FusionFileChangeHandler,
+	PhpFileChangeHandler,
+	XlfFileChangeHandler,
+	YamlFileChangeHandler
 ]
 
 export class LanguageServer extends Logger {
@@ -52,6 +63,7 @@ export class LanguageServer extends Logger {
 	protected clientCapabilityService: ClientCapabilityService
 
 	protected functionalityInstances: Map<new (...args: unknown[]) => AbstractFunctionality, AbstractFunctionality> = new Map()
+	protected fileChangeHandlerInstances: Map<new (...args: unknown[]) => AbstractFileChangeHandler, AbstractFileChangeHandler> = new Map()
 
 	constructor(connection: _Connection, documents: TextDocuments<FusionDocument>) {
 		super()
@@ -68,22 +80,27 @@ export class LanguageServer extends Logger {
 
 		this.addFunctionalityInstance(InlayHintLanguageFeature)
 		this.addFunctionalityInstance(SemanticTokensLanguageFeature)
+
+		this.addFunctionalityInstance(FusionFileChangeHandler)
+		this.addFunctionalityInstance(PhpFileChangeHandler)
+		this.addFunctionalityInstance(XlfFileChangeHandler)
+		this.addFunctionalityInstance(YamlFileChangeHandler)
 	}
 
 	protected addFunctionalityInstance(type: new (...args: unknown[]) => AbstractFunctionality) {
 		this.functionalityInstances.set(type, new type(this))
 	}
 
-	public getFunctionalityInstance<T extends AbstractFunctionality>(type: new (...args: unknown[]) => AbstractFunctionality): T | undefined {
+	public getFunctionalityInstance<T extends AbstractFunctionality>(type: new (...args: unknown[]) => T): T | undefined {
 		return <T | undefined>this.functionalityInstances.get(type)
 	}
 
-	public runCapability<T extends AbstractCapability>(type: new (...args: unknown[]) => AbstractCapability, params: any) {
+	public runCapability<T extends AbstractCapability>(type: new (...args: unknown[]) => T, params: any) {
 		const capability = this.getFunctionalityInstance<T>(type)
 		return capability ? capability.execute(params) : undefined
 	}
 
-	public runLanguageFeature<T extends AbstractLanguageFeature>(type: new (...args: unknown[]) => AbstractLanguageFeature, params: any) {
+	public runLanguageFeature<T extends AbstractLanguageFeature>(type: new (...args: unknown[]) => T, params: any) {
 		const languageFeature = this.getFunctionalityInstance<T>(type)
 		return languageFeature ? languageFeature.execute(params) : undefined
 	}
@@ -207,109 +224,13 @@ export class LanguageServer extends Logger {
 	}
 
 	public async onDidChangeWatchedFiles(params: DidChangeWatchedFilesParams) {
-		// TODO: Create separate Watchers (like capabilities)
 		// TODO: Update relevant ParsedFusionFiles but check if it was not a change the LSP does know of
 		for (const change of params.changes) {
-			// console.log(`CHANGE: ${change.type} ${change.uri}`)
 			this.logVerbose(`Watched: (${Object.keys(FileChangeType)[Object.values(FileChangeType).indexOf(change.type)]}) ${change.uri}`)
-			if (change.type === FileChangeType.Changed) await this.handleFileChanged(change)
-			if (change.type === FileChangeType.Created) await this.handleFileCreated(change)
-			if (change.type === FileChangeType.Deleted) await this.handleFileDeleted(change)
-		}
-	}
-
-	protected async handleNodeTypeFileChanged() {
-		for (const workspace of this.fusionWorkspaces) {
-			for (const neosPackage of workspace.neosWorkspace.getPackages().values()) {
-				neosPackage.readConfiguration()
+			for (const fileChangeHandlerType of FileChangeHandlerTypes) {
+				const fileChangeHandler = this.getFunctionalityInstance(fileChangeHandlerType)
+				await fileChangeHandler.tryToHandle(change)
 			}
-		}
-		await Promise.all(this.fusionWorkspaces.map(workspace => workspace.diagnoseAllFusionFiles()))
-	}
-
-	protected async handleFileChanged(change: FileEvent) {
-		if ((change.uri.endsWith(".yaml") || change.uri.endsWith(".yml")) && change.uri.includes("NodeTypes")) {
-			await this.handleNodeTypeFileChanged()
-		}
-
-		if (change.uri.endsWith(".xlf")) {
-			clearLineDataCacheForFile(change.uri)
-			for (const workspace of this.fusionWorkspaces) {
-				const translationFile = workspace.getTranslationFileByUri(change.uri)
-				if (!translationFile) continue
-				translationFile.parse().catch(error => this.logError("handleFileChanged", error))
-			}
-		}
-
-		if (change.uri.endsWith(".php")) {
-			clearLineDataCacheForFile(change.uri)
-
-			for (const workspace of this.fusionWorkspaces) {
-				for (const neosPackage of workspace.neosWorkspace.getPackages().values()) {
-					const helper = neosPackage.getEelHelpers().find(helper => helper.uri === change.uri)
-					if (!helper) continue
-
-					this.logVerbose(`  File was EEL-Helper ${helper.name}`)
-
-					const namespace = helper.namespace
-					const classDefinition = namespace.getClassDefinitionFromFilePathAndClassName(uriToPath(helper.uri), helper.className, helper.pathParts)
-
-					this.logVerbose(`  Methods: then ${helper.methods.length} now ${classDefinition.methods.length}`)
-
-					helper.methods = classDefinition.methods
-					helper.position = classDefinition.position
-				}
-			}
-		}
-	}
-
-	protected async handleFileCreated(change: FileEvent) {
-		if ((change.uri.endsWith(".yaml") || change.uri.endsWith(".yml")) && change.uri.includes("NodeTypes")) {
-			await this.handleNodeTypeFileChanged()
-		}
-
-		if (change.uri.endsWith(".xlf")) {
-			const workspace = this.getWorkspaceForFileUri(change.uri)
-			if (!workspace) return
-
-			const neosPackage = workspace.neosWorkspace.getPackageByUri(change.uri)
-			if (!neosPackage) return
-
-			const basePath = neosPackage.getTranslationsBasePath()
-			const filePath = uriToPath(change.uri)
-
-			const translationFile = XLIFFTranslationFile.FromFilePath(neosPackage, filePath, basePath)
-			await translationFile.parse()
-			workspace.translationFiles.push(translationFile)
-		}
-
-		if (change.uri.endsWith(".fusion")) {
-			const workspace = this.getWorkspaceForFileUri(change.uri)
-			if (!workspace) {
-				this.logInfo(`Created Fusion file corresponds to no workspace. ${change.uri}`)
-				return
-			}
-
-			const neosPackage = workspace.neosWorkspace.getPackageByUri(change.uri)
-			workspace.addParsedFileFromPath(uriToPath(change.uri), neosPackage)
-			this.logDebug(`Added new ParsedFusionFile ${change.uri}`)
-		}
-	}
-
-	protected async handleFileDeleted(change: FileEvent) {
-		clearLineDataCacheForFile(change.uri)
-
-		if ((change.uri.endsWith(".yaml") || change.uri.endsWith(".yml")) && change.uri.includes("NodeTypes")) {
-			await this.handleNodeTypeFileChanged()
-		}
-
-		if (change.uri.endsWith(".fusion")) {
-			const workspace = this.getWorkspaceForFileUri(change.uri)
-			if (!workspace) {
-				this.logInfo(`Deleted Fusion file corresponds to no workspace. ${change.uri}`)
-				return
-			}
-			workspace.removeParsedFile(change.uri)
 		}
 	}
 
---
+++ server/src/fileChangeHandler/PhpFileChangeHandler.ts
@@ -0,0 +1,39 @@
+import { FileChangeType, FileEvent } from 'vscode-languageserver';
+import { AbstractFileChangeHandler } from './AbstractFileChangeHandler';
+import { clearLineDataCacheForFile, uriToPath } from '../common/util';
+
+export class PhpFileChangeHandler extends AbstractFileChangeHandler {
+	canHandleFileEvent(fileEvent: FileEvent): boolean {
+		this.logInfo(`canHandleFileEvent: ${fileEvent.type === FileChangeType.Changed}`)
+		return fileEvent.type === FileChangeType.Changed && fileEvent.uri.endsWith(".php")
+	}
+
+	public async handleChanged(fileEvent: FileEvent) {
+		clearLineDataCacheForFile(fileEvent.uri)
+		this.logInfo(`handle change of file: ${fileEvent.uri}`)
+		for (const workspace of this.languageServer["fusionWorkspaces"]) {
+			for (const neosPackage of workspace.neosWorkspace.getPackages().values()) {
+				const helper = neosPackage.getEelHelpers().find(helper => helper.uri === fileEvent.uri)
+				if (!helper) continue
+
+				this.logInfo(`  File was EEL-Helper ${helper.name}`)
+
+				const namespace = helper.namespace
+				const classDefinition = namespace.getClassDefinitionFromFilePathAndClassName(uriToPath(helper.uri), helper.className, helper.pathParts)
+
+				this.logInfo(`  Methods: then ${helper.methods.length} now ${classDefinition.methods.length}`)
+
+				helper.methods = classDefinition.methods
+				helper.position = classDefinition.position
+			}
+		}
+	}
+
+	public async handleCreated(fileEvent: FileEvent) {
+		this.logError('handleCreated: Method not implemented.');
+	}
+
+	public async handleDeleted(fileEvent: FileEvent) {
+		this.logError('handleDeleted: Method not implemented.');
+	}
+}
