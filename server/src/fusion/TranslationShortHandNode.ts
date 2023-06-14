b628df143b6fcdffbc9b44d9827cb145c286aaa8
sjsone <simon@sjs.one>
2023-06-15 00:53:18 +0200

    chore: translation autocompletion

--- server/src/fusion/TranslationShortHandNode.ts
+++ server/src/fusion/TranslationShortHandNode.ts
@@ -1,5 +1,6 @@
 import { AbstractNode } from 'ts-fusion-parser/out/common/AbstractNode';
 import { LiteralStringNode } from 'ts-fusion-parser/out/dsl/eel/nodes/LiteralStringNode';
+import { ShortHandIdentifier, XLIFFService } from '../common/XLIFFService';
 
 declare module 'ts-fusion-parser/out/dsl/eel/nodes/LiteralStringNode' {
 	interface LiteralStringNode {
@@ -8,11 +9,18 @@ declare module 'ts-fusion-parser/out/dsl/eel/nodes/LiteralStringNode' {
 }
 
 export class TranslationShortHandNode extends AbstractNode {
+	public shortHandIdentifier?: ShortHandIdentifier
+
 	constructor(
 		public literalStringNode: LiteralStringNode
 	) {
 		super(literalStringNode["position"], literalStringNode["parent"])
 		literalStringNode["translationShortHandNode"] = this
+	}
+
+	getShortHandIdentifier() {
+		if (!this.shortHandIdentifier) this.shortHandIdentifier = XLIFFService.readShortHandIdentifier(this.getValue())
+		return this.shortHandIdentifier
 	}
 
 	getValue() {
