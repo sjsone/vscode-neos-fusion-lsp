2616750188936dae8f06c548ea086849f5280048
sjsone <simon@sjs.one>
2023-07-30 20:00:06 +0200

    chore: created IgnorableDiagnostic

---
+++ src/diagnostics/IgnorableDiagnostic.ts
@@ -0,0 +1,27 @@
+import { Diagnostic, Range, DiagnosticSeverity, integer, DiagnosticRelatedInformation } from 'vscode-languageserver'
+
+export interface IgnorableDiagnostic extends Diagnostic {
+	data: {
+		quickAction: "ignorable"
+	}
+}
+
+export namespace IgnorableDiagnostic {
+	/**
+	 * Creates a new IgnorableDiagnostic literal.
+	 */
+	export function create(range: Range, message: string, severity?: DiagnosticSeverity, code?: integer | string, source?: string, relatedInformation?: DiagnosticRelatedInformation[]) {
+		const diagnostic = <IgnorableDiagnostic>Diagnostic.create(range, message, severity, code, source, relatedInformation)
+		diagnostic.data = {
+			quickAction: "ignorable"
+		}
+		return diagnostic
+	};
+
+	/**
+	 * Checks whether the given literal conforms to the {@link IgnorableDiagnostic} interface.
+	 */
+	export function is(value: any): value is IgnorableDiagnostic {
+		return Diagnostic.is(value) && value.data?.quickAction === "ignorable"
+	}
+}
