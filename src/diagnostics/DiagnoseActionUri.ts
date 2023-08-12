48bf7defb762bb855cb7357b0e0897400eff878e
sjsone <simon@sjs.one>
2023-08-12 12:04:57 +0200

    chore: cleanup

--- src/diagnostics/DiagnoseActionUri.ts
+++ src/diagnostics/DiagnoseActionUri.ts
@@ -14,14 +14,12 @@ export function diagnoseActionUri(parsedFusionFile: ParsedFusionFile) {
 		const actionNameNode = actionUriActionNode.getNode().name
 		const actionName = actionNameNode.value
 
-		if (actionName.endsWith("Action")) {
-			diagnostics.push({
-				severity,
-				range: actionNameNode.linePositionedNode.getPositionAsRange(),
-				message: `Neos would interpret this as "${actionName}Action". Remove "Action" from the name.`,
-				source: CommonDiagnosticHelper.Source
-			})
-		}
+		if (actionName.endsWith("Action")) diagnostics.push({
+			severity,
+			range: actionNameNode.linePositionedNode.getPositionAsRange(),
+			message: `Neos would interpret this as "${actionName}Action". Remove "Action" from the name.`,
+			source: CommonDiagnosticHelper.Source
+		})
 	}
 
 	const actionUriControllerNodes = parsedFusionFile.getNodesByType(ActionUriControllerNode) ?? []
@@ -29,14 +27,12 @@ export function diagnoseActionUri(parsedFusionFile: ParsedFusionFile) {
 		const actionNameNode = actionUriControllerNode.getNode().name
 		const actionName = actionNameNode.value
 
-		if (actionName.endsWith("Controller")) {
-			diagnostics.push({
-				severity,
-				range: actionNameNode.linePositionedNode.getPositionAsRange(),
-				message: `Neos would interpret this as "${actionName}Controller". Remove "Controller" from the name.`,
-				source: CommonDiagnosticHelper.Source
-			})
-		}
+		if (actionName.endsWith("Controller")) diagnostics.push({
+			severity,
+			range: actionNameNode.linePositionedNode.getPositionAsRange(),
+			message: `Neos would interpret this as "${actionName}Controller". Remove "Controller" from the name.`,
+			source: CommonDiagnosticHelper.Source
+		})
 	}
 
 	return diagnostics
