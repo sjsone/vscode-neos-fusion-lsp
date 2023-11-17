6f7e24efc840a6ba7c377f27a91cb998c5675223
sjsone <simon@sjs.one>
2023-11-17 23:36:17 +0100

    todo: rewrite mergeObjects

--- src/common/util.ts
+++ src/common/util.ts
@@ -122,6 +122,7 @@ export function isPrototypeDeprecated(workspace: FusionWorkspace, prototypeName:
 }
 
 export function mergeObjects(source: unknown, target: unknown) {
+    // TODO: rewrite mergeObjects
     // https://gist.github.com/ahtcx/0cd94e62691f539160b32ecda18af3d6?permalink_comment_id=3889214#gistcomment-3889214
     for (const [key, val] of Object.entries(source)) {
         if (val !== null && typeof val === `object`) {
