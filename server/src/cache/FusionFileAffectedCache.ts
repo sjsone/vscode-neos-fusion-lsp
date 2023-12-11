3df2b74cfff3b3c4cfc264976d8bc29e2b6f956c
sjsone <simon@sjs.one>
2023-12-11 15:54:10 +0100

    chore: added cache

---
+++ server/src/cache/FusionFileAffectedCache.ts
@@ -0,0 +1,9 @@
+import { Cache } from './Cache';
+import { CacheManager } from './CacheManager';
+
+export class FusionFileAffectedCache<T> extends Cache<T> {
+	constructor(logSuffix?: string) {
+		super(logSuffix)
+		CacheManager.registerFusionFileAffectedCache(this)
+	}
+}
