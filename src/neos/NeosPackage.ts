5b0dabb80b0aa4063e11ea5930bcf6c2a4600ffe
sjsone <simon@sjs.one>
2023-12-30 19:09:15 +0100

    chore: fixed neos package name building

--- src/neos/NeosPackage.ts
+++ src/neos/NeosPackage.ts
@@ -135,13 +135,13 @@ export class NeosPackage extends Logger {
 		return this.eelHelpers
 	}
 
-	getName() {
+	getName(): string {
 		return this.composerJson.name
 	}
 
 	getPackageName() {
 		const packageKey = this.composerJson.extra?.neos?.["package-key"]
-		return packageKey ?? this.getName().split("/").map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('.')
+		return packageKey ?? this.getName().split(/[\/-]+/).map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('.')
 	}
 
 	hasName(name: string) {
