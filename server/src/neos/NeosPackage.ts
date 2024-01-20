06972ccd92a65d4fb772b99677bce1f56d96c255
sjsone <simon@sjs.one>
2024-01-20 16:27:51 +0100

    chore: cleanup

--- server/src/neos/NeosPackage.ts
+++ server/src/neos/NeosPackage.ts
@@ -141,7 +141,7 @@ export class NeosPackage extends Logger {
 
 	getPackageName() {
 		const packageKey = this.composerJson.extra?.neos?.["package-key"]
-		return packageKey ?? this.getName().split(/[\/-]+/).map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('.')
+		return packageKey ?? this.getName().split(/[/-]+/).map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('.')
 	}
 
 	hasName(name: string) {
