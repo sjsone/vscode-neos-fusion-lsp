b97a91becaa264f7d3924249036c22e609ceb93f
sjsone <simon@sjs.one>
2024-05-25 17:45:16 +0200

    chore: `TranslationService.readTranslationsFromPackage` is now async

--- src/common/TranslationService.ts
+++ src/common/TranslationService.ts
@@ -4,15 +4,19 @@ import { XLIFFTranslationFile } from '../translations/XLIFFTranslationFile'
 import { getFiles } from './util'
 
 class TranslationService {
-	readTranslationsFromPackage(neosPackage: NeosPackage) {
+	async readTranslationsFromPackage(neosPackage: NeosPackage) {
 		const basePath = neosPackage.getTranslationsBasePath()
 		if (!NodeFs.existsSync(basePath)) return []
+
 		const translationFilePaths = Array.from(getFiles(basePath, ".xlf"))
-		return translationFilePaths.map(filePath => {
+
+		const translationFiles = translationFilePaths.map(async filePath => {
 			const translationFile = XLIFFTranslationFile.FromFilePath(neosPackage, filePath, basePath)
-			translationFile.parse().catch(error => { })
+			await translationFile.parse()
 			return translationFile
 		})
+
+		return Promise.all(translationFiles)
 	}
 }
 
