b16e71679e55bf828b4c0730e7974934e41a922c
sjsone <simon@sjs.one>
2024-06-22 12:49:20 +0200

    chore: cleanup

--- server/src/fusion/node/FqcnNode.ts
+++ server/src/fusion/node/FqcnNode.ts
@@ -1,17 +1,12 @@
 import { AbstractNode } from 'ts-fusion-parser/out/common/AbstractNode'
-import { NodePosition, NodePositionStub } from 'ts-fusion-parser/out/common/NodePosition'
+import { NodePosition } from 'ts-fusion-parser/out/common/NodePosition'
 import { ClassDefinition } from '../../neos/NeosPackageNamespace'
 
 export class FqcnNode extends AbstractNode {
-	public identifier: string
-	public classDefinition: ClassDefinition
 	readonly realLength: number
 
-	constructor(identifier: string, classDefinition: ClassDefinition, position: NodePosition) {
-		super(NodePositionStub)
-		this.identifier = identifier
-		this.classDefinition = classDefinition
-		this.position = position
+	constructor(public identifier: string, public classDefinition: ClassDefinition, position: NodePosition) {
+		super(position)
 
 		this.realLength = identifier.length + identifier.split('\\').length - 1
 	}
