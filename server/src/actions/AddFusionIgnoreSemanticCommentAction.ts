5da3a6a199d70462de12e66642468d460ae72ed9
sjsone <simon@sjs.one>
2024-07-16 18:46:29 +0200

    fix: handle file not found in addFusionIgnoreSemanticCommentAction

--- server/src/actions/AddFusionIgnoreSemanticCommentAction.ts
+++ server/src/actions/AddFusionIgnoreSemanticCommentAction.ts
@@ -4,7 +4,12 @@ import { LanguageServer } from '../LanguageServer'
 import { getLinesFromLineDataCacheForFile, hasLineDataCacheFile, setLinesFromLineDataCacheForFile, uriToPath } from '../common/util'
 
 async function getLinesFromUri(uri: string) {
-	const document = (await NodeFs.promises.readFile(uriToPath(uri))).toString()
+	const file = await NodeFs.promises.readFile(uriToPath(uri))
+	if (!file) return undefined
+
+	const document = file.toString()
+	if (!document) return undefined
+
 	return document.split("\n")
 }
 
@@ -15,7 +20,13 @@ export const addFusionIgnoreSemanticCommentAction = async (languageServer: Langu
 	for (const diagnostic of params.context.diagnostics) {
 		if (diagnostic.data?.quickAction !== "ignorable") continue
 
-		if (!hasLineDataCacheFile(uri)) setLinesFromLineDataCacheForFile(uri, await getLinesFromUri(uri))
+		if (!hasLineDataCacheFile(uri)) {
+			const lines = await getLinesFromUri(uri)
+			if (lines === undefined) continue
+
+			setLinesFromLineDataCacheForFile(uri, lines)
+		}
+
 		const entry = getLinesFromLineDataCacheForFile(uri)
 		if (!entry) continue
 
