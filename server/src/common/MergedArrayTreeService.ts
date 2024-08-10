d92f0b34cfeb1bffecdf88807027035b0bbf9a5b
sjsone <simon@sjs.one>
2024-08-10 12:03:50 +0200

    chore: improved `MergedArrayTreeService.abstractNodeToMergedArrayTreePath`

--- server/src/common/MergedArrayTreeService.ts
+++ server/src/common/MergedArrayTreeService.ts
@@ -1,7 +1,9 @@
+import path = require('path')
 import { AbstractNode } from 'ts-fusion-parser/out/common/AbstractNode'
 import { ObjectNode } from 'ts-fusion-parser/out/dsl/eel/nodes/ObjectNode'
 import { ObjectPathNode } from 'ts-fusion-parser/out/dsl/eel/nodes/ObjectPathNode'
 import { MetaPathSegment } from 'ts-fusion-parser/out/fusion/nodes/MetaPathSegment'
+import { ObjectPath } from 'ts-fusion-parser/out/fusion/nodes/ObjectPath'
 import { ObjectStatement } from 'ts-fusion-parser/out/fusion/nodes/ObjectStatement'
 import { PathSegment } from 'ts-fusion-parser/out/fusion/nodes/PathSegment'
 import { PrototypePathSegment } from 'ts-fusion-parser/out/fusion/nodes/PrototypePathSegment'
@@ -12,13 +14,25 @@ class MergedArrayTreeService {
 		const nodePath: string[] = []
 		let foundNode: AbstractNode | undefined = node
 		do {
-			nodePath.unshift(...this.abstractNodeToMergedArrayTreePath(foundNode))
-			foundNode = foundNode.parent
+			const result = this.abstractNodeToMergedArrayTreePath(foundNode)
+			nodePath.unshift(...result.paths)
+			foundNode = result.nextNode
 		} while (foundNode)
 		return nodePath
 	}
 
-	protected *abstractNodeToMergedArrayTreePath(node: AbstractNode): Generator<string> {
+	protected abstractNodeToMergedArrayTreePath(node: AbstractNode) {
+		let nextNode: AbstractNode | undefined = node.parent
+		let paths: Array<string> = []
+		if (node instanceof PathSegment) {
+			const objectPath = node.parent as ObjectPath
+			const nodeIndex = objectPath.segments.indexOf(node)
+			const segmentsUntilNode = objectPath.segments.slice(0, nodeIndex + 1)
+			// node.parent  => ObjectPath
+			// node.parent.parent => ObjectStatement
+			nextNode = node.parent?.parent?.parent
+			paths = segmentsUntilNode.map(n => n.identifier)
+		}
 		// if (node instanceof ObjectPathNode) {
 		// 	const objectNode = node.parent
 		// 	if (objectNode instanceof ObjectNode) {
@@ -35,11 +49,11 @@ class MergedArrayTreeService {
 
 		if (node instanceof ObjectStatement) {
 			for (const segment of node["path"]["segments"]) {
-				if (segment instanceof PathSegment) yield segment.identifier
-				if (segment instanceof PrototypePathSegment) yield `<${segment.identifier}>`
+				if (segment instanceof PathSegment) paths.push(segment.identifier)
+				if (segment instanceof PrototypePathSegment) paths.push(`<${segment.identifier}>`)
 				if (segment instanceof MetaPathSegment) {
-					yield "__meta"
-					yield segment.identifier
+					paths.push("__meta")
+					paths.push(segment.identifier)
 				}
 			}
 		}
@@ -59,6 +73,11 @@ class MergedArrayTreeService {
 
 
 		// yield `<todo:${node.constructor.name}>`
+
+		return {
+			nextNode,
+			paths
+		}
 	}
 }
 
