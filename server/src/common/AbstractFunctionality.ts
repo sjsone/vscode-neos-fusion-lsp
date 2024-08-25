29efe73832d10b1edf1f670e2faeb2d9a6de534f
sjsone <simon@sjs.one>
2024-08-25 16:43:07 +0200

    chore: removed server

--- server/src/common/AbstractFunctionality.ts
+++
@@ -1,9 +0,0 @@
-import { LanguageServer } from '../LanguageServer'
-import { Logger } from './Logging'
-
-
-export class AbstractFunctionality extends Logger {
-	constructor(protected languageServer: LanguageServer) {
-		super()
-	}
-}
--- server/src/error/ControllableError.ts
+++
@@ -1,3 +0,0 @@
-export abstract class ControllableError extends Error {
-
-}
